20950f74065d6746c97dbf22f3f6369d01034729
rafaelcairess <rafaelcairespires@gmail.com>
2026-05-17 11:54:48 -0300

    organiza KPIs em colunas

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import sqlite3
 import pandas as pd 
 
-#subir o servidor streamlit e printar uma tabela com os dados do db
+# Conecta ao banco de dados
 def create_conection():
     return sqlite3.connect('banco_redes_sociais.db')
 
@@ -10,18 +10,23 @@ conn = create_conection()
 
 df = pd.read_sql_query('SELECT * FROM tb_uso_redes_sociais;', conn)
 
-# st.write(df)
+# Título
+st.title("Redes Sociais e Bem-Estar Emocional")
+st.divider()
 
-# calcula a media dos valores somando tudo e dividindo pela quantidade 
-st.write(df)
+# Calcula as métricas
 media_tempo_uso = df['Daily_Usage_Time (minutes)'].mean()
 emocoes_dominantes = df['Dominant_Emotion'].mode()
 media_engajamento = df['Likes_Received_Per_Day'].mean()
 
-st.metric(label="Tempo de uso médio", value=round(media_tempo_uso, 2))
-st.metric(label="Emoções dominantes", value=emocoes_dominantes[0])
-st.metric(label="Curtidas por dia", value=round(media_engajamento, 2))
+# Exibe os KPIs em colunas
+col1, col2, col3 = st.columns(3)
 
+with col1:
+    st.metric(label="Tempo de uso médio", value=f"{round(media_tempo_uso, 1)} min")
 
-st.write(f"Média: {media_tempo_uso}\nEmoção dominante: {emocoes_dominantes[0]} media de engajamento {media_engajamento}")
+with col2:
+    st.metric(label="Emoção mais frequente", value=emocoes_dominantes[0])
 
+with col3:
+    st.metric(label="Curtidas por dia", value=round(media_engajamento, 1))
