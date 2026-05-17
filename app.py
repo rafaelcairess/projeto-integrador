9abd7ca03cb26ccfcf3b6d93d6a62d244da593d6
vitmgomez <vit.mgomez@gmail.com>
2026-05-17 13:16:14 -0300

    Adiciona grafico eixo 1 wip(work in progress)

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import sqlite3
 import pandas as pd 
+import plotly.express as px
 
 #subir o servidor streamlit e printar uma tabela com os dados do db
 def create_conection():
@@ -12,16 +13,25 @@ df = pd.read_sql_query('SELECT * FROM tb_uso_redes_sociais;', conn)
 
 # st.write(df)
 
-# calcula a media dos valores somando tudo e dividindo pela quantidade 
+# .mean() calcula a media dos valores somando tudo e dividindo pela quantidade 
 st.write(df)
 media_tempo_uso = df['Daily_Usage_Time (minutes)'].mean()
 emocoes_dominantes = df['Dominant_Emotion'].mode()
 media_engajamento = df['Likes_Received_Per_Day'].mean()
 
+# Criação de cards
 st.metric(label="Tempo de uso médio", value=round(media_tempo_uso, 2))
 st.metric(label="Emoções dominantes", value=emocoes_dominantes[0])
 st.metric(label="Curtidas por dia", value=round(media_engajamento, 2))
 
+#Eixo 1 - Associação por plataforma
+df_contagem = df.groupby(['Platform', 'Dominant_Emotion']).size().reset_index(name='Contagem')
 
-st.write(f"Média: {media_tempo_uso}\nEmoção dominante: {emocoes_dominantes[0]} media de engajamento {media_engajamento}")
-
+st.bar_chart(
+    data=df_contagem,
+    x="Platform",
+    y="Contagem",
+    color="Dominant_Emotion",
+    x_label="Plataforma",
+    y_label="Quantidade de Emoções"
+)
